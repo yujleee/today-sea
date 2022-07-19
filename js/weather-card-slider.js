347b294dce721dccfdbd13930c88e552f5997b93
이유정 <yujlee242@gmail.com>
2022-07-19 22:10:30 +0900

    🚧  카드 슬라이드를 위한 tiny-slider.js 추가

---
+++ js/weather-card-slider.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+const weatherSlider = tns({
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  container: '.card-wrap',
+  controls: false,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+});

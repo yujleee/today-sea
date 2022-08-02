efb0923320ef73585de0fcb50d953df32bbd352d
이유정 <yujlee242@gmail.com>
2022-08-02 17:24:46 +0900

    🚧  API 쿼리스트링에 필요한 값 수정

--- js/api.js
+++ js/api.js
@@ -6,20 +6,21 @@ const API_KEY = 'JgOTEFegmT85gM1vQ7XNalEJFJb0gusFB26pkZkBameKaU3B5WlTltcyt6xWvGR
 const now = {};
 const currentTime = async () => {
   const today = new Date();
-  if (today.getMonth() + 1 < 9) {
-    now.date = `${today.getFullYear()}0${today.getMonth() + 1}${today.getDate()}`;
-  } else {
-    now.date = `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}`;
-  }
-  now.time = `${today.getHours() + 2}00`;
+  const year = today.getFullYear();
+  const month = today.toLocaleString('ko-KO', { month: '2-digit' }).slice(0, 2);
+  const date = today.toLocaleString('ko-KO', { day: '2-digit' }).slice(0, 2);
+  now.date = `${year}${month}${date}`;
+  now.time = `${today.getHours()}00`;
   return now;
 };
 
 const App = async () => {
   currentTime();
+  const beachInfo = JSON.parse(localStorage.getItem('beachInfo'));
+  const beachNumber = beachInfo.num;
 
-  const vilageFcstBeach = await fetch(
-    `${URL}/getVilageFcstBeach?serviceKey=${API_KEY}&dataType=JSON&base_date=${now.date}&base_time=${now.time}&beach_num=1`
+  await fetch(
+    `${URL}/getVilageFcstBeach?serviceKey=${API_KEY}&dataType=JSON&base_date=${now.date}&base_time=${now.time}&beach_num=${beachNumber}`
   )
     .then((response) => response.json())
     .then((data) => render(data))

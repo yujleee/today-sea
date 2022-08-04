93fb050b02006154a07c8d1710bb662fa0f3513c
이유정 <yujlee242@gmail.com>
2022-08-04 17:27:58 +0900

    🎨  풍향 구간에 따라 표현단위 국문으로 출력하도록 추가

--- js/renderFcstInfo.js
+++ js/renderFcstInfo.js
@@ -1,9 +1,32 @@
+/* eslint-disable no-restricted-syntax */
 import { selectImg } from './weatherImg.js';
 
 const beachInfo = JSON.parse(localStorage.getItem('beachInfo'));
 
 const beachInfoTop = document.querySelector('.beach-info-top');
 const infoArea = document.querySelector('.info-area');
+
+const windDirectionType = {
+  45: '북-북동',
+  90: '북동-동',
+  135: '동-남동',
+  180: '남동-남',
+  225: '남-남서',
+  270: '남서-서',
+  315: '남-북서',
+  360: '북서-북',
+};
+
+const getWindDirection = (vce) => {
+  let windType = '';
+
+  for (const [deg, type] of Object.entries(windDirectionType)) {
+    if (vce <= deg) {
+      windType = type;
+    }
+  }
+  return windType;
+};
 
 export const renderFcstInfo = (pos) => {
   const getData = (data) => {
@@ -23,5 +46,5 @@ export const renderFcstInfo = (pos) => {
   infoArea.querySelector('.humidty').innerText = `${weather.REH}%`;
   infoArea.querySelector('.wave').innerText = `${weather.WAV}`;
   infoArea.querySelector('.wind-speed').innerText = `${weather.WSD}`;
-  infoArea.querySelector('.wind-direction').innerText = `${weather.VEC}`;
+  infoArea.querySelector('.wind-direction').innerText = `${getWindDirection(weather.VEC)}`;
 };

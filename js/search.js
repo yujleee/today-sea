b3308710ff7cdaa4455ce7e781c162ebc329fe36
이유정 <yujlee242@gmail.com>
2022-09-14 22:03:58 +0900

    🐛 검색 관련 이슈 처리 검색어 키보드 컨트롤 버그 픽스, 검색결과 없을 시 텍스트 노출

--- js/search.js
+++ js/search.js
@@ -4,6 +4,8 @@ import { store } from './store/store.js';
 function Search() {
   const BASE_TIME = [2, 5, 8, 11, 14, 17, 20, 23];
   const searchInput = document.querySelector('.input-search');
+  const submitButton = document.querySelector('.search-btn');
+  const errorText = document.querySelector('.error-text');
   const searchForm = searchInput.parentNode;
   const resultList = document.querySelector('.result-list');
 
@@ -62,18 +64,39 @@ function Search() {
     let beachList = [];
     if (word !== '') {
       const recommandName = beachNames.filter((name) => name.toLowerCase().startsWith(word));
-      beachList = recommandName.map((item) => `<li>${item}</li>`).join('');
+      beachList = recommandName.map((item, idx) => `<li data-index=${idx}>${item}</li>`).join('');
     }
+
+    if (beachList.length === 0) {
+      resultList.parentNode.classList.remove('is-active');
+      errorText.classList.add('is-active');
+      return;
+    }
+
+    resultList.parentNode.classList.add('is-active');
     resultList.innerHTML = beachList;
     this.searchMaxCount = resultList.childElementCount;
   };
 
-  const submitBeach = (e) => {
-    const beachName = e.target.innerText;
-    let beachNumber = 1;
+  const submitBeach = (e, activeText) => {
+    const beachName = activeText || e.target.innerText;
+    let beachNumber = -1;
+
     for (const [idx, beach] of Object.entries(beachInfo)) {
+      //   beachNumber = beach.name === beachName ? beach['beach-num'] : -1;
       if (beach.name === beachName) {
         beachNumber = beach['beach-num'];
+      }
+    }
+    try {
+      if (beachNumber === -1) {
+        throw new SyntaxError('검색 결과가 존재하지 않습니다.');
+      }
+    } catch (error) {
+      if (error.name === 'SyntaxError') {
+        errorText.classList.add('is-active');
+        errorText.innerText = error.message;
+        return;
       }
     }
 
@@ -98,7 +121,8 @@ function Search() {
       this.searchCount = this.searchMaxCount;
     }
 
-    const li = resultList.childNodes[this.searchCount];
+    const li = resultList.childNodes;
+
     switch (e.key) {
       case 'ArrowUp':
         this.searchCount -= 1;
@@ -107,32 +131,48 @@ function Search() {
       case 'ArrowDown':
         this.searchCount += 1;
         break;
-      case 'Enter':
-        searchInput.value = li.innerText;
-        submitBeach();
-        break;
       default:
         break;
     }
-    li.classList.toggle('is-active');
+
+    Array.from(li).forEach((item) => {
+      const idx = Number(item.getAttribute('data-index'));
+      if (idx === this.searchCount) {
+        item.classList.add('is-active');
+      } else {
+        item.classList.remove('is-active');
+      }
+    });
   };
 
-  searchInput.addEventListener('keyup', (e) => {
-    if (e.key !== 'backspace') {
-      resultList.parentNode.classList.add('is-active');
+  searchInput.addEventListener('input', (e) => {
+    resultList.parentNode.classList.remove('is-active');
+    if (e.target.value.length !== 0) {
       handleSearch();
     }
+  });
 
+  searchInput.addEventListener('keyup', (e) => {
     if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
       handleSearchResult(e);
     }
   });
 
   searchInput.addEventListener('keydown', (e) => {
+    errorText.classList.remove('is-active');
     if (e.key === 'Enter') {
       e.preventDefault();
-      handleSearchResult(e);
+
+      const li = resultList.childNodes;
+      const activeText = Array.from(li).find((item) => item.classList.contains('is-active'));
+      //   this.target.value = activeText.innerText;
+      submitBeach(e, activeText.innerText);
     }
+  });
+
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    submitBeach(e);
   });
 
   resultList.addEventListener('click', (e) => {
